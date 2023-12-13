a68ab89aec0a695127174270631042c45c66af99
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-12-13 16:11:44 -0500

    components and base setup

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-
-    </>
+    <div>
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
---
+++ portfolio-react/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='felx items-center justify-center flex-col text-center pt-20 pb-6'>
+        <h1 className='text-3xl md:text-5xl mb-1 md:mb-3 font-bold'>Ryan Fitzgerald</h1>
+        <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer</p>
+        <p className='text-sm mb-6 font-bold'>This is my BIO</p>
+    </div>
+  )
+}
+
+export default About
