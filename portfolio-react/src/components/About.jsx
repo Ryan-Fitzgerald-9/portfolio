6af81d67a606c24db41d54ba29c814850155c52c
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2024-01-02 14:13:54 -0500

    about updated

--- portfolio-react/src/components/About.jsx
+++ portfolio-react/src/components/About.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id="home" className='flex items-center text-white bg-gradient-to-b from-gray-900 to-white justify-center flex-col text-center pt-20 pb-6'>
-        <h1 className='text-3xl md:text-5xl mb-1 md:mb-3 font-bold'>Ryan Fitzgerald</h1>
-        <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer</p>
-        <p className='text-sm max-w-xl mb-6 font-bold'>A former senior accountant turned junior developer. My interest in process improvement and problem-solving has led to a passion for debugging and creating projects. </p>
+    <div id="home" className='my-[100px] pl-[50px] flex-col pt-20 pb-6'>
+        <h1 className='text-3xl md:text-5xl mb-1 md:mb-3 font-bold'>Hi there, I'm Ryan!</h1>
+        <p className='text-base md:text-xl mb-3 font-medium'>Former Senior Accountant turned Software Engineer</p>
+        <p className='text-sm text-gray-400 max-w-xl mb-6 font-bold'>I'm a self-motivated software engineer located in Blackstone, MA who enjoys solving challenging programming problems and debugging code. My 12+ years as an accountant enables me to approach projects with high attention to detail, process improvement, and an organizational mindset. After achieving my Software Engineering Immersive Certificate from General Assembly, I'm now ready for new opportunities as a developer.</p>
     </div>
   )
 }
