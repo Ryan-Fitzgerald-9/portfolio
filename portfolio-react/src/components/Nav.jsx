1db44730ad19ef64df147cffadcdd43e1d3a4530
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2024-01-03 13:14:59 -0500

    tailwind updates for mobile

--- portfolio-react/src/components/Nav.jsx
+++ portfolio-react/src/components/Nav.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-scroll'
 const Nav = () => {
 
   return (
-    <div className="flex justify-center bg-gray-800 h-[50px] p-4 fixed top-0 left-0 right-0 z-50">
-      <div className='container ml-[30px] text-white flex items-center justify-start text-xl md:text-3xl font-bold hover:cursor-pointer'>
+    <div className="flex flex-col justify-center md:flex-row md:items-center bg-gray-800 h-[50px] p-4 fixed top-0 left-0 right-0 z-50">
+      <div className='container md:ml-[30px] text-white flex md:items-center justify-center md:justify-start text-xl md:text-3xl font-bold hover:cursor-pointer'>
         <Link to="home" spy={true} smooth={true} offset={-100} duration={500} className="pr-5">Ryan Fitzgerald</Link>
       </div>
       <div className='container mx-auto text-stone-300 flex items-center justify-evenly text-base md:text-xl font-bold hover:cursor-pointer'>
